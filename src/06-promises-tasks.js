33343a597ab224927a67e2e0d0c0a6947a0ccba5
Irina Migunova <ista.irina@gmail.com>
2023-01-02 01:02:11 +0300

    fix: try to fix bug with score

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -27,18 +27,8 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(isPositiveAnswer) {
-  let result;
-  if (isPositiveAnswer === true) {
-    result = Promise.resolve('Hooray!!! She said "Yes"!');
-  } else if (isPositiveAnswer === false) {
-    result = Promise.resolve('Oh no, she said "No".');
-  } else {
-    result = Promise.reject(
-      new Error('Wrong parameter is passed! Ask her again.'),
-    );
-  }
-  return result;
+function willYouMarryMe(/* isPositiveAnswer */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -56,15 +46,8 @@ function willYouMarryMe(isPositiveAnswer) {
  *    })
  *
  */
-function processAllPromises(array) {
-  const res = [];
-  array.map((elem) => {
-    elem.then((tes) => {
-      res.push(tes);
-    });
-    return elem;
-  });
-  return Promise.resolve(res);
+function processAllPromises(/* array */) {
+  throw new Error('Not implemented');
 }
 
 /**
@@ -86,8 +69,8 @@ function processAllPromises(array) {
  *    })
  *
  */
-function getFastestPromise(array) {
-  return Promise.race(array);
+function getFastestPromise(/* array */) {
+  throw new Error('Not implemented');
 }
 
 /**
